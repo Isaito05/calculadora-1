a3d9b6cea0809a6a1bb2a405db6ba4e6c92110c1
Isaito05 <deleondiazisaacdavid@gmail.com>
2022-10-19 15:06:47 -0500

    ISAAC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-primer-proyecto';
+  constructor(){}
   
+  pant1 = "";
+  pant2 = "";
+  operador="";
+  ress="";
+  
+  num1(){
+    this.pant1 = this.pant1+ "1";
+  }
+
+  num2(){
+    this.pant1 = this.pant1+ "2";
+  }
+
+  num3(){
+    this.pant1 = this.pant1+ "3";
+  }
+  
+  num4(){
+    this.pant1 = this.pant1+ "4";
+  }
+
+  num5(){
+    this.pant1 = this.pant1+ "5";
+  }
+
+  num6(){
+    this.pant1 = this.pant1+ "6";
+  }
+
+  num7(){
+    this.pant1 = this.pant1+ "7";
+  }
+
+  num8(){
+    this.pant1 = this.pant1+ "8";
+  }
+
+  num9(){
+    this.pant1 = this.pant1+ "9";
+  }
+
+  suma(){
+    if(this.operador==""){
+      this.operador="+";
+      this. pant2 = this.pant1;
+      this.pant1 = "";
+    }
+    else{
+      this.operador="+";
+    }
+  }
+
+  igual(){
+    switch(this.operador){
+      case "+":
+      // this.ress=parseFloat(this.pant1)+parseFloat(this.pant2);
+        this.pant2="";
+        this.pant1=this.ress.toString();
+        this.operador="";
+        break;
+      }
+  }
 }
